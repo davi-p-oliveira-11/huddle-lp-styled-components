a613c1952638e1958f3c7e1c6778eb219e39e834
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-04 20:09:00 -0300

    added images folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import { StyledHeader } from "./styles/Header.styled"
+import { Container } from "./styles/Container.styled"
 
 const Header = () => {
   return (
     <StyledHeader> 
-      <h1>Huddle</h1>
+      <Container>
+       <h1>Huddle</h1>
+      </Container>
     </StyledHeader>
   )
 }
