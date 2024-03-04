2e489357268c9826099244bc767a120d4a2f4e0e
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-04 20:29:41 -0300

    fixed logo path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
-import { StyledHeader } from "./styles/Header.styled"
+import { StyledHeader, Nav, Logo } from "./styles/Header.styled"
 import { Container } from "./styles/Container.styled"
-
+import { Button } from "./styles/Button.styled"
+import  LogoImg  from "../images/logo.svg"
 
 const Header = () => {
   return (
     <StyledHeader> 
       <Container>
        <Nav>
-         <Logo src='./images/logo.svg' />
+         <Logo src={LogoImg} alt='Logo'/>
          <Button>Try it Free</Button>
        </Nav>
       </Container>
