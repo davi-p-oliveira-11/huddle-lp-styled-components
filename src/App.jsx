e2f355da2aa4980fa2ef9c2dc4733c7599cc4542
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-04 18:23:55 -0300

    fixed problem in the main jsx and added more components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Header from './components/Header'
+import { Container } from './components/styles/Container.styled'
+
 function App() {
-  
   return (
+    <>
+    <Header />
     <Container>
-      <h1>Hello Mars</h1>
+      <h1>Hellssso Mars</h1>
     </Container>
+    </>
   )
 }
 
