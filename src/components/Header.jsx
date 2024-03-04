02873f5e0c59ab17f4b10a62a5e801825669883e
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-04 20:56:59 -0300

    added styles to the img and imported to the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
-import { StyledHeader, Nav, Logo } from "./styles/Header.styled"
+import { StyledHeader, Nav, Logo, Image } from "./styles/Header.styled"
 import { Container } from "./styles/Container.styled"
 import { Flex } from "./styles/Flex.styled"
 import { Button } from "./styles/Button.styled"
 import  LogoImg  from "../images/logo.svg"
+import  Mockup from "../images/illustration-mockups.svg"
 
 const Header = () => {
   return (
@@ -16,9 +17,17 @@ const Header = () => {
        <Flex>
         <div>
           <h1>
-            
+            Build The Community Your Fans Will Love
           </h1>
+
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique obcaecati nihil esse nam dignissimos facilis assumenda adipisci quas ea est dolore repellendus aliquid eligendi reprehenderit, labore aperiam, amet molestiae neque officia id voluptates? Amet id totam reiciendis aliquid molestias?</p>
+
+          <Button bg='#ff0099' color='#fff'>
+            Get Started For Free
+          </Button>
         </div>
+
+        <Image src={Mockup} />
        </Flex>
       </Container>
     </StyledHeader>
--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -21,3 +21,13 @@ export const Logo = styled.img`
     margin-bottom: 40px;
   }
 `
+
+
+export const Image = styled.img`
+  width: 375px;
+  margin-left: 40px;
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    margin: 40px 0 30px;
+  }
+`
