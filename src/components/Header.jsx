2cc02b4834264bea87743eb68a91b3eea5d0c528
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-04 20:40:52 -0300

    added flex component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { StyledHeader, Nav, Logo } from "./styles/Header.styled"
 import { Container } from "./styles/Container.styled"
+import { Flex } from "./styles/Flex.styled"
 import { Button } from "./styles/Button.styled"
 import  LogoImg  from "../images/logo.svg"
 
@@ -11,6 +12,14 @@ const Header = () => {
          <Logo src={LogoImg} alt='Logo'/>
          <Button>Try it Free</Button>
        </Nav>
+
+       <Flex>
+        <div>
+          <h1>
+            
+          </h1>
+        </div>
+       </Flex>
       </Container>
     </StyledHeader>
   )
