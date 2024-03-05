74b3a86493847156d3bb04423408e96c06c94179
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-04 22:15:30 -0300

    finally rendered the images in the cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,17 @@
+import { StyledCard } from './styles/Card.styled'
 
 
-const Card = ({item: {id, title, body, image}}) => {
+export default function Card({ item: { id, title, body, image } }) {
   return (
-    <div>
+    <StyledCard layout={id % 2 === 0 && 'row-reverse'}>
       <div>
         <h2>{title}</h2>
         <p>{body}</p>
       </div>
 
       <div>
-        <img src={`./images/${image}`} alt="" />
+        <img src={`../src/images/${image}`} alt='' />
       </div>
-    </div>
+    </StyledCard>
   )
 }
-
-export default Card
